718786d0fd217b88a592d9c5e534900bf44562c1
KtKeaton <ktr01@protonmail.com>
2021-12-03 21:37:37 +0800

    確立Task之CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,52 @@
 class TasksController < ApplicationController
+  def home
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new  
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @task = find_task
+  end
+
+  def edit
+    @task = find_task
+  end
+
+  def update
+    @task = find_task
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = find_task
+
+    if Time.now < find_task.date
+      @task.destroy
+      redirect_to tasks_url, :notice => "任務已刪除"
+    else
+      redirect_to tasks_url, :notice => "任務已過期，不可刪除"
+    end
+  end
+  
+private
+  def find_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :due_date, :note)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :tasks
-  root "tasks#index"
+  root "tasks#home"
   
 
 end
