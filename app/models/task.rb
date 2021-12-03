7cbfea464f596c4913779ae2fa9554c2045e10e2
KtKeaton <ktr01@protonmail.com>
2021-12-03 20:45:56 +0800

    確立Model一對多、多對多關係及其驗證

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
-  belongs_to :user
+  validates :name, :note, presence: true
+  has_many :category_tasks
+  has_many :categories, through: :category_tasks
+	belongs_to :user, foreign_key: true
 end
