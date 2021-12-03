b05b8e0a13a80621616b64bb1e372d6e8443d45e
KtKeaton <ktr01@protonmail.com>
2021-12-03 21:45:25 +0800

    暫時關閉User必填

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,5 @@ class Task < ApplicationRecord
   validates :name, :note, presence: true
   has_many :category_tasks
   has_many :categories, through: :category_tasks
-	belongs_to :user, foreign_key: true
+	belongs_to :user, foreign_key: true, optional: true
 end
---
+++ db/migrate/20211203133916_change_tasks_column_user_id.rb
@@ -0,0 +1,5 @@
+class ChangeTasksColumnUserId < ActiveRecord::Migration[6.1]
+  def change
+    change_column_null :tasks, :user_id, true
+  end
+end
