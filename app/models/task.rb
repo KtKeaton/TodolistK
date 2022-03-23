36d9fe657e8e22bed5e2882e50eeca9ea5298240
KtKeaton <ktr01@protonmail.com>
2022-03-23 09:54:17 +0800

    Model 預防性驗證 status與priority

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
-  validates :name, :note, :due_date, presence: true
+  validates :name, :note, :due_date, :status, :priority, presence: true
   has_many :category_tasks
   has_many :categories, through: :category_tasks
 	belongs_to :user, foreign_key: true, optional: true
