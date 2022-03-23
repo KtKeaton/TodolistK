2237c37508f7f0949c709b0a3a97d27fc6224ef4
KtKeaton <ktr01@protonmail.com>
2022-03-23 12:21:52 +0800

    新增 Routes及Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   resources :tasks, path: "mission_possible" do
   end
+
+  resources :users, only: [:create] do
+    collection do
+      get :sign_up
+      get :sign_in
+    end
+  end
+
   root "tasks#index"
+
 end
