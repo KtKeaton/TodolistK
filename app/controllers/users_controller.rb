f105a331bdb13f2684dacf1bfbd7b6f9d6753fd1
KtKeaton <ktr01@protonmail.com>
2022-03-29 04:11:48 +0800

    修正User role屬性

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-        redirect_to "/"
+        redirect_to list_tasks_path
     else
         render :sign_up
     end
---
+++ db/migrate/20220328200759_change_role_to_user.rb
@@ -0,0 +1,5 @@
+class ChangeRoleToUser < ActiveRecord::Migration[6.1]
+  def change
+    change_column :users, :role, :string
+  end
+end
