e47754bebf8aadef5b063789acabc4cb04efc20e
KtKeaton <ktr01@protonmail.com>
2022-04-04 13:31:57 +0800

    調整Turbo

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,8 @@ module TodolistK
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.i18n.default_locale = "zh-TW"
-
+    config.action_view.form_with_generates_remote_forms = false
+    
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
