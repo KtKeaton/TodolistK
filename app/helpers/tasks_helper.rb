d62367b56bc4fb496942e9c554df6499553a1094
KtKeaton <ktr01@protonmail.com>
2022-04-10 16:24:12 +0800

    WIP 最愛任務 View及model 且User encrypt_password 移入private

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,11 @@
 module TasksHelper
+  def favorite_icon(user, task)
+    classes = ['favorite_icon']
+
+    if user
+      classes << (user.favorite?(task) ? 'favorite-on' : 'favorite-off')
+    end
+
+    tag.div class: classes, "data-favorite-target": "icon"
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,17 @@ class User < ApplicationRecord
   validates :role, presence: true, uniqueness: true
   has_many :tasks, dependent: :destroy
   has_many :bookmarks
-  
+  has_many :favorite_notes,
+            through: :bookmarks,
+            source: :task
+
   before_create :encrypt_password
 
+  def favorite?(n)
+    favorite_notes.exists?(n.id)
+  end
+
+private
   def encrypt_password
     salted_pw = "eiojre#{self.password}rewropwr"
     self.password = Digest::SHA1.hexdigest(salted_pw)
