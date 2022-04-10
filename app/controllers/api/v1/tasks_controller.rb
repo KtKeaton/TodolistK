e6d474491b4abf7f4905f78ee7c6f52e7d4a7311
KtKeaton <ktr01@protonmail.com>
2022-04-10 15:49:34 +0800

    WIP 最愛任務controller

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::TasksController < ApplicationController
+    def favorite
+    note = Note.find(params[:id])
+
+    if Bookmark.exists?(note: note)
+      # 移除最愛
+      current_user.favorite_notes.delete(note)
+      render json: { status: "removed", id: params[:id] }
+    else
+      # 新增最愛
+      current_user.favorite_notes << note
+      render json: { status: "added", id: params[:id] }
+    end
+  end
+end
